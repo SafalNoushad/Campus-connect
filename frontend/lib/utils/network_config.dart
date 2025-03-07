6f50f4fb6f18227617c4f4adc93c77b5c5f163f6
Safal Noushad <safalnoushad2005@gmail.com>
2025-03-08 00:07:44 +0530

    added the requirement.txt file

--- frontend/lib/utils/network_config.dart
+++ frontend/lib/utils/network_config.dart
@@ -7,7 +7,7 @@ class NetworkConfig {
     } else if (Platform.isIOS) {
       return "http://localhost:5001"; // ✅ iOS Simulator
     } else {
-      return "http://192.168.1.181:5001"; // ✅ Change this to your local backend IP
+      return "http://192.168.1.7:5001"; // ✅ Change this to your local backend IP
     }
   }
 }
